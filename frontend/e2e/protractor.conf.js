b2f10b007b8cda16138467274e7a15bb1cb7bca8
Salvatore Iovene <salvatore@iovene.com>
2018-12-13 23:24:11 +0100

    (e2e) Fix spec reporter declaration

--- frontend/e2e/protractor.conf.js
+++ frontend/e2e/protractor.conf.js
@@ -1,7 +1,7 @@
 // Protractor configuration file, see link for more information
 // https://github.com/angular/protractor/blob/master/lib/config.ts
 
-var SpecReporter = require('jasmine-spec-reporter');
+var SpecReporter = require('jasmine-spec-reporter').SpecReporter;
 
 exports.config = {
   allScriptsTimeout: 11000,
