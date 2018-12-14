4e9f9cecc73964acb5acea761dc6f8f2ed34efb7
Salvatore Iovene <salvatore@iovene.com>
2018-12-15 00:37:21 +0100

    (frontend) Add header/footer skeletons and initial styles

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,16 +1,21 @@
-import {BrowserModule} from '@angular/platform-browser';
-import {NgModule} from '@angular/core';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
-import {AppRoutingModule} from './app-routing.module';
-import {AppComponent} from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LibraryModule } from "./library/library.module";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    // Angular
     BrowserModule,
-    AppRoutingModule
+
+    // App
+    AppRoutingModule,
+    LibraryModule
   ],
   providers: [],
   bootstrap: [AppComponent]
